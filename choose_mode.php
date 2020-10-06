7237e4c34f41442aea601c6462bfbed70a8e29e6
Lea Moser <lea.moser@stud.fhgr.ch>
2020-10-06 10:51:19 +0200

    edited infoscreen and choosemode

--- choose_mode.php
+++ choose_mode.php
@@ -3,14 +3,17 @@ $titel = 'Wähle deinen Modus';
 //Button 1
 $b1_titel = 'Lerne das Spiel';
 $b1_btn = 'start';
+$b1_link = '/tutorial.php';
 //Button 2
 $b2_titel = 'Entdeckermodus';
 $b2_btn = 'start';
+$b2_link = '/infoscreen_freeplay.php';
 //Button 3
 $b3_titel = 'Spielmodus';
 $b3_btn1 = 'easy';
 $b3_btn2 = 'medium';
 $b3_btn3 = 'hard';
+$b3_link = '/infoscreen_spielmodus.php';
 ?>
 
 <?php include('templates/head.php') ?>
@@ -21,18 +24,18 @@ $b3_btn3 = 'hard';
     <article>
         <div>
             <h2><?php echo $b1_titel ?></h2>
-            <button class="full"><?php echo $b1_btn ?></button>
+            <button class="full"><a href="<?php echo $b1_link ?>"><?php echo $b1_btn ?></a></button>
         </div>
         <div>
             <h2><?php echo $b2_titel ?></h2>
-            <button class="full"><?php echo $b2_btn ?></button>
+            <button class="full"><a href="<?php echo $b2_link ?>"><?php echo $b2_btn ?></a></button>
         </div>
         <div>
             <h2><?php echo $b3_titel ?></h2>
             <div class="flex">
-                <button class="full"><?php echo $b3_btn1 ?></button>
-                <button class="full"><?php echo $b3_btn2 ?></button>
-                <button class="full"><?php echo $b3_btn3 ?></button>
+                <button class="full"><a href="<?php echo $b3_link ?>"><?php echo $b3_btn1 ?></a></button>
+                <button class="full"><a href="<?php echo $b3_link ?>"><?php echo $b3_btn2 ?></a></button>
+                <button class="full"><a href="<?php echo $b3_link ?>"><?php echo $b3_btn3 ?></a></button>
             </div>
         </div>
     </article>
