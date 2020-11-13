009ebf3b95682791e8a9644aea8c53f068b8d6bd
Lea Moser <lea.moser@stud.fhgr.ch>
2020-11-13 13:48:59 +0100

    delete fonts

--- js/modules/updateCockpit.js
+++ js/modules/updateCockpit.js
@@ -4,7 +4,7 @@ let writeContainer = (time, placeStart) => {
 
 let updateCockpit = (time, placeStart) => {
     let journeyInfoContainer = document.querySelector('#journeyInfo');
-    console.log()
+    console.log(journeyInfoContainer);
     writeContainer(time, placeStart);
     // stationsContainer.innerHTML += placeStart + ',' + time + ')' + ', ';
 }
