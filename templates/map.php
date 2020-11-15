26827497b9464f3f17ceb402b22a5add64088cb7
Lea Moser <lea.moser@stud.fhgr.ch>
2020-11-15 18:55:53 +0100

    adapt colors map-icons

--- templates/map.php
+++ templates/map.php
@@ -2,13 +2,28 @@
     <div id="map"></div>
     <div id="mapButtons">
         <div id="mapButtonMountain" class="hide">
-            <img src="../assets/icons/mountain.svg" alt="toggle mountain layer" width="100%">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 800 800" style="enable-background:new 0 0 800 800;" xml:space="preserve">
+                <polygon class="st0" points="313.6,12 463.3,396 613,780 313.6,780 14.2,780 163.9,396 " />
+                <polygon class="st1" points="597.1,461.5 689.1,620.7 781,780 597.1,780 413.2,780 505.2,620.7 " />
+            </svg>
         </div>
         <div id="mapButtonWater" class="hide">
-            <img src="../assets/icons/water.svg" alt="toggle water layer" width="100%">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 800 800" style="enable-background:new 0 0 800 800;" xml:space="preserve">
+                <polygon class="st0" points="331,195.6 171.8,287.6 12.5,379.5 12.5,195.6 12.5,11.7 171.8,103.7 " />
+                <path class="st1" d="M482.5,382.5c-165.1,0-299-187-299-187s133.9-187,299-187s299,187,299,187S647.6,382.5,482.5,382.5z" />
+                <path class="st0" d="M311.5,408.5c165.1,0,299,187,299,187s-133.9,187-299,187s-299-187-299-187S146.4,408.5,311.5,408.5z" />
+                <polygon class="st1" points="463,595.4 622.2,503.4 781.5,411.5 781.5,595.4 781.5,779.3 622.2,687.3 " />
+            </svg>
         </div>
         <div id="mapButtonCity" class="hide">
-            <img src="../assets/icons/city.svg" alt="toggle city layer" width="100%">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 800 800" style="enable-background:new 0 0 800 800;" xml:space="preserve">
+                <g>
+                    <rect x="21.2" y="25" class="st0" width="485.8" height="755" />
+                </g>
+                <g>
+                    <rect x="400" y="398" class="st1" width="380" height="380" />
+                </g>
+            </svg>
         </div>
     </div>
 </article>
