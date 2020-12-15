c06b67455a1404e4a2d34ead169fe3e346cd4d3c
Lea Moser <lea.moser@stud.fhgr.ch>
2020-12-15 14:09:01 +0100

    pwa upload

--- about.php
+++ about.php
@@ -1,8 +1,10 @@
 <?php include('templates/head.php') ?>
 <section class="maincontainer" id="contentpage">
     <h1>About</h1>
-    <p>Wir sind Lea und Alice, zwei Multimedia Production Studentinnen aka Webentwicklerinnen aus der Ostschweiz. Unser Game ‘<?php echo $gametitle ?>’ ist im Rahmen eines Abschlussprojekts an der FHGR entstanden.</p>
-    <p>Falls du Fragen hast, erreichst du uns über:</p>
+    <p>Wir sind Lea und Alice, zwei Multimedia Production Studentinnen aka Webentwicklerinnen aus der Ostschweiz.</p>
+    <p>Unser Game ‘<?php echo $gametitle ?>’ ist im Rahmen eines Abschlussprojekts an der FHGR entstanden.</p>
+    <img src="assets/images/us.png">
+    <p>Falls du Fragen hast, erreichst du uns per Mail.</p>
     <p><a href="mailto:privat@lea-moser.ch">privat@lea-moser.ch</a></p>
     <p><a href="mailto:aliceloher@gmx.ch">aliceloher@gmx.ch</a></p>
 </section>
