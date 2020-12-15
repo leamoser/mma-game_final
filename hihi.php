5c36320615bca948cf65b62ccf40bd9a32eed4d5
Lea Moser <lea.moser@stud.fhgr.ch>
2020-12-15 17:35:13 +0100

    hihi edits

--- hihi.php
+++ hihi.php
@@ -1,6 +1,9 @@
 <?php include('templates/head.php') ?>
-    <section class="maincontainer" id="contentpage">
-        <h1>hesch s'gfühl du bisch lustig...</h1>
-        <a href="/freeplay"><button class="full"><p>jetzt aber richtig bitte</p></button></a>
-    </section>
+<section class="maincontainer hihi" id="contentpage">
+    <iframe src="https://giphy.com/embed/a9xhxAxaqOfQs" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <h1>hesch s'gfühl du bisch lustig...</h1>
+    <a href="/freeplay"><button class="full">
+            <p>jetzt aber richtig bitte</p>
+        </button></a>
+</section>
 <?php include('templates/foot.php') ?>
